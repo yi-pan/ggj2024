c867bc9c688b4015ea097fb0d7edcc17e5e783f6
yi-pan <61952114+yi-pan@users.noreply.github.com>
2024-01-28 01:55:42 -0500

    plane game 75%

--- Make Me Laugh/Assets/Scripts/CandyGame.cs
+++ Make Me Laugh/Assets/Scripts/CandyGame.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class CandyGame : MonoBehaviour
 {
+    public Camera game_cam;
+
     private bool isPlaying;
+    private RaycastHit raycastHit;
+
+    
     public void gameStart()
     {
         isPlaying = true;
-       
     }
 
     // Update is called once per frame
@@ -16,7 +21,13 @@ public class CandyGame : MonoBehaviour
     {
         if (isPlaying)
         {
-
+            // raycast
+            Ray ray = game_cam.ScreenPointToRay(Input.mousePosition);
+            // hover
+            if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit))
+            {
+                
+            }
         }
     }
 }
